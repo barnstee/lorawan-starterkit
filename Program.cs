7e48915b0c6459794c0c441a05982ff195b98740
Erich Barnstedt <erichb@microsoft.com>
2025-05-09 10:51:37 +0200

    Moved LoRaWAN Network Server to OPC Foundation UA Edge Translator GitHub repo and removed tests, as they no longer apply.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,4 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using LoRaWan.NetworkServer;
-using LoRaWan.NetworkServer.BasicsStation;
-
-using var cts = new CancellationTokenSource();
-var cancellationToken = cts.Token;
-
-var configuration = NetworkServerConfiguration.CreateFromEnvironmentVariables();
-await BasicsStationNetworkServer.RunServerAsync(configuration, cancellationToken);
-
 Thread.Sleep(Timeout.Infinite); // Keep the process alive
--- Tests/E2E/README.md
+++
@@ -1,3 +0,0 @@
-# E2E Tests
-
-This file has been moved to: <https://github.com/Azure/iotedge-lorawan-starterkit/blob/docs/main/docs/testing/e2e_tests.md>
